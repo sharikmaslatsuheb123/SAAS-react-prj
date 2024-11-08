abe69432b6a034668d62b6fb5117486973e367bb
sharikmaslatsuheb123 <sharikmaslatsuheb@gmail.com>
2024-11-08 17:47:05 +0530

    remove commented code from sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,29 +1,4 @@
-// import React from 'react';
-// import './Sidebar.css';
 
-// const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
-//   return (
-//     <div className={`sidebar-container ${isSidebarOpen ? 'open' : ''}`}>
-//       <button className="sidebar-arrow" onClick={toggleSidebar}>
-//         {isSidebarOpen ? '<' : '>'}
-//       </button>
-//       <div className={`custom-offcanvas ${isSidebarOpen ? 'open' : ''}`}>
-//         {/* <div className="offcanvas-header">
-//           <button type="button" className="btn-close" onClick={toggleSidebar} aria-label="Close"></button>
-//         </div> */}
-//         <div className="offcanvas-body">
-//           <p><i className="bi bi-speedometer2"></i> Overview</p>
-//           <p><i className="bi bi-person"></i> Dashboard</p>
-//           <p><i className="bi bi-gear"></i> Orders</p>
-//           <p><i className="bi bi-envelope"></i> Services</p>
-//           <p><i className="bi bi-envelope"></i> Customers</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
 import React from 'react';
 import { NavLink } from 'react-router-dom'; // Use NavLink for active styling
 import './Sidebar.css';
